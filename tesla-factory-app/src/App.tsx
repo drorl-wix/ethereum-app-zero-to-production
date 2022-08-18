e3c7723ba585adb714492d2e31d29b382a83e758
Dror Lapidot <46896703+drorl-wix@users.noreply.github.com>
2022-08-18 08:30:03 +0300

    Update App.tsx

--- tesla-factory-app/src/App.tsx
+++ tesla-factory-app/src/App.tsx
@@ -7,13 +7,13 @@ function App() {
   const [isCreating, setIsCreating] = useState<boolean>(false);
   const [transactionHash, setTransactionHash] = useState<string>('');
 
-  const onCreateTeslaCarClick = async () => {
+  const onBuyTeslaClick = async () => {
     setIsCreating(true);
 
     try {
       const teslaFactoryContract = await getTeslaFactoryContract();
-      const createCarCall = await teslaFactoryContract.createTesla();
-      const transaction = await createCarCall.wait();
+      const buyTeslaCall = await teslaFactoryContract.buyTesla();
+      const transaction = await buyTeslaCall.wait();
       setTransactionHash(transaction.transactionHash);
     } finally {
       setIsCreating(false);
@@ -22,11 +22,11 @@ function App() {
 
   const getTeslaFactoryContract = async (): Promise<Contract> => {
     const abi = [
-      "function createTesla() public",
+      "function buyTesla() public",
     ];
 
     const signer = await getSignerFromMetaMask();
-    const contractAddress = "0x2f991C16E94694C6CF2668eC008D30d6f2787396";
+    const contractAddress = "0x91D851a9aE3FAe94dD74B21e00908EBe8cFbf1B6"; // Replace it with your contract address
 
     return new Contract(contractAddress, abi, signer);
   }
@@ -44,7 +44,7 @@ function App() {
           <img src="https://www.leaseplan.com/-/media/leaseplan-digital/int/business/images/car-spotlight-pages/tesla/tesla-3/tesla-model-3-image-1.png?iar=0&rev=44add536550041e5b55cac337bb9c481&mw=1300" alt=""/>
           {(isCreating) && <TailSpin color="#fcba03" ariaLabel="tail-spin-loading"/>}
 
-          {!isCreating && <div className="button" onClick={onCreateTeslaCarClick}>Get Tesla</div>}
+          {!isCreating && <div className="button" onClick={onBuyTeslaClick}>Buy Tesla</div>}
           {transactionHash && <a href={`https://goerli.etherscan.io/tx/${transactionHash}`} target="_blank" rel="noreferrer"> View in Etherscan </a>}
         </header>
       </div>
@@ -52,4 +52,3 @@ function App() {
 }
 
 export default App;
-
