f2a6e9ce17c12781ef56984137959845db1adb97
Dror Lapidot <46896703+drorl-wix@users.noreply.github.com>
2022-08-18 08:28:57 +0300

    Update TeslaFactory.sol

--- solidity/TeslaFactory.sol
+++ solidity/TeslaFactory.sol
@@ -4,24 +4,29 @@ pragma solidity ^0.8.16;
 contract TeslaFactory {
     int private teslaId;
 
+    A type that contains collection of variables
     struct Tesla { 
         int id;
         string model;
     }
 
-    mapping(address => Tesla) private owners;
+    // Key value mapping between owner's address and Tesla
+    mapping(address => Tesla) private buyers;
 
-    event TeslaCreared(address owner, Tesla tesla);
+    event TeslaSold(address buyer, Tesla tesla);
 
-    function createTesla() public {
+    function buyTesla() public {
         teslaId++;
+
+        // memory variable will mark this data as temporary and will be used only inside this function
         Tesla memory tesla = Tesla(teslaId, "Model X");
 
-        owners[msg.sender] = tesla;
-        emit TeslaCreared(msg.sender, tesla);
+        // msg is a global variable contains access properties (e.g. caller address, etc.)
+        buyers[msg.sender] = tesla;
+        emit TeslaSold(msg.sender, tesla);
     }        
 
     function hasTesla() public view returns (bool) {
-        return owners[msg.sender].id > 0;
+        return buyers[msg.sender].id > 0;
     }
 }
